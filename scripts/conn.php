d562ac0c72a796555891a45ff30b82a559321663
wwwxkz <wxkzwin@gmail.com>
2020-09-09 14:18:22 -0300

    Test variables removed from conn.php

--- scripts/conn.php
+++ scripts/conn.php
@@ -1,5 +1,4 @@
 <?php
-	$teste = "tesssste";
 	$conn = new PDO('mysql: host=localhost; dbname=company;','root','');
 	// function createConn($company, $user, $password){
 	//     $conn = new PDO("mysql: host=localhost; dbname=" . $company . ";",$user,$password);
